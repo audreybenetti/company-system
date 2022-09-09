734675a640653637fd0fa6b86abb7fea06266708
audreybenetti <audrey.sestari@gmail.com>
2022-09-09 10:15:43 -0300

    Initial commit

---
+++ src/test/java/br/edu/utfpr/CompanyApplicationTest.java
@@ -0,0 +1,20 @@
+package br.edu.utfpr;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class CompanyApplicationTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
